548a8dcbd657c5247b4ae8ce9af7ff0fc8251043
RajasriJanaki <rajasrijr55@gmail.com>
2023-07-16 23:43:33 -0700

    feat: add projects

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Header from './components/Header';
 import './App.css';
 import Landing from './components/Landing';
 import Footer from './components/Footer';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Landing />
+        <Projects />
         <Footer />
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,2 +1,19 @@
-export default function Projects() {
-}
+import {
+    Box,
+    Container,
+    Stack,
+    Text,
+    Link,
+  } from '@chakra-ui/react';
+import Carousel from './Carousel';
+
+  export default function Projects() {
+    return (
+      <Box
+        textAlign={'center'}
+        bg={'#438496'}
+        color={'#DFDCE3'} h={'100vh'} pd="100">
+          <Carousel />
+      </Box>
+    );
+  }
